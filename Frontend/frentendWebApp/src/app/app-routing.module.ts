087c415a7889e22227505bc0abbc55ad995db194
OumaimaYounsi <92877970+OumaimaYounsi@users.noreply.github.com>
2023-08-16 07:16:57 +0100

    Updates
     Database connection + front structure

--- Frontend/frentendWebApp/src/app/app-routing.module.ts
+++ Frontend/frentendWebApp/src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { FrontLayoutComponent } from './layouts/front-layout/front-layout.compon
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { DashboardModule } from './views/admin/dashboard/dashboard.module';
 import { AppComponent } from './app.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 const routes: Routes = [
   {path:'', component:FrontLayoutComponent, children:[
     {path:'', loadChildren:()=>import('./views/front/home-page/home-page.module').then(m=>m.HomePageModule)},
-    {path:'loginuser', loadChildren:()=>import('./views/front/userlogin/userlogin.module').then(m=>m.UserloginModule)},
+    // {path:'loginuser', loadChildren:()=>import('./views/front/userlogin/userlogin.module').then(m=>m.UserloginModule)},
     {path:'about', loadChildren:()=>import('./views/front/about/about.module').then(m=>m.AboutModule)},
     {path:'prodacts', loadChildren:()=>import('./views/front/prodacts/prodacts.module').then(m=>m.ProdactsModule)},
     {path:'contact', loadChildren:()=>import('./views/front/contact/contact.module').then(m=>m.ContactModule)}
@@ -16,8 +17,16 @@ const routes: Routes = [
   {path:'admin',component:AdminLayoutComponent, children:[
     {path:'', loadChildren:()=>import('./views/admin/dashboard/dashboard.module').then(m=>m.DashboardModule)},
     {path:'loginadmin', loadChildren:()=>import('./views/admin/login-admin/login-admin.module').then(m=>m.LoginAdminModule)},
-    {path:'contactliste', loadChildren:()=>import('./views/admin/contact-liste/contact-liste.module').then(m=>m.ContactListeModule)}
+    {path:'contactliste', loadChildren:()=>import('./views/admin/contact-liste/contact-liste.module').then(m=>m.ContactListeModule)},
+    // {path:'login', loadChildren:()=>import('./views/admin/login-admin/login-admin.module').then(m=>m.LoginAdminModule)},
+    {path:'prodactsliste', loadChildren:()=>import('./views/admin/prodact-liste/prodact-liste.module').then(m=>m.ProdactListeModule)},
+    {path:'userliste', loadChildren:()=>import('./views/admin/user-liste/user-liste.module').then(m=>m.UserListeModule)}
 
+  ]},
+  {path:'login', component:AuthLayoutComponent, children:[
+    {path:'', loadChildren:()=>import('./views/admin/login-admin/login-admin.module').then(m=>m.LoginAdminModule)},
+    {path:'register', loadChildren:()=>import('./views/admin/register/register.module').then(m=>m.RegisterModule)},
+    {path:'resetpassword', loadChildren:()=>import('./views/admin/reset-password/reset-password.module').then(m=>m.ResetPasswordModule)}
   ]}
   // {path:"admin", loadChildren:()=>import('./views/admin/dashboard/dashboard.module').then(m=>m.DashboardModule)}
 ];
--- Frontend/frentendWebApp/src/app/layouts/layouts.module.ts
+++ Frontend/frentendWebApp/src/app/layouts/layouts.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { FrontLayoutComponent } from './front-layout/front-layout.component';
 import { RouterModule } from '@angular/router';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 
 
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
-    FrontLayoutComponent
+    FrontLayoutComponent,
+    AuthLayoutComponent
   ],
   imports: [
     CommonModule,
